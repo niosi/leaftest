bab78dfe0c9160dc94f0e5cf9074c2be541133c9
qiyamiky@qq.com <NICKY>
2018-09-29 09:23:17 +0800

    1

--- leafserver/src/server/login/internal/handler.go
+++ leafserver/src/server/login/internal/handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/name5566/leaf/gate"
 	"reflect"
 	"server/msg"
+	"time"
 )
 
 func handleMsg(m interface{}, h interface{}) {
@@ -17,18 +18,18 @@ func init() {
 }
 
 func handleTest(args []interface{}) {
-	fmt.Printf("%#v\n", args)
+	fmt.Printf("request start time:%s\n", time.Now().Format("2006-01-02 15:04:05"))
 	// 收到的 Test 消息
 	m := args[0].(*msg.Login)
 	// 消息的发送者
 	a := args[1].(gate.Agent)
 	// 输出收到的消息的内容
-	fmt.Printf("%#v\n%#v\n", m, a)
-
+	fmt.Printf("User:%s,Pwd:%s\n")
 	if m.LoginName == "testUser" && m.LoginPwd == "testPwd" {
 		// 给发送者回应一个消息
 		a.WriteMsg(&msg.Resp{Code: 0, Msg: "succ"})
 	} else {
 		a.WriteMsg(&msg.Resp{Code: 10000, Msg: "fail"})
 	}
+	fmt.Printf("request end time:%s\n", time.Now().Format("2006-01-02 15:04:05"))
 }
